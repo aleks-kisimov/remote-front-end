1785bb53a303af474b2e1b930d096ec708f29aa5
alex-lidell <48863415+alex-lidell@users.noreply.github.com>
2022-11-10 10:22:58 +0000

    Update PropertyListing.jsx
    Updated property list results to be fetched from API endpoint

--- src/components/PropertyListing/PropertyListing.jsx
+++ src/components/PropertyListing/PropertyListing.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import PropertyCard from '../PropertyCard';
 import './PropertyListing.scss';
 
-const DUMMY_PROPERTY = {
-    id: 73864112,
-    bedrooms: 3,
-    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
-    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
-    propertyType: 'Flat',
-    price: 1950000,
-    branchName: 'M2 Property, London',
-    propertyUrl: '/property-for-sale/property-73864112.html',
-    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
-    propertyTitle: '3 bedroom flat for sale',
-    mainImage:
-        'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
-};
-
 const PropertyListing = () => {
+    const pageSize = 5;
+    const [propertyList, setPropertyList] = useState([]);
+
+    const paginateResults = (results, page) => {
+        const pageStart = (page - 1) * pageSize;
+        const pageEnd = page * pageSize;
+        const currentPage = results.slice(pageStart, pageEnd);
+
+        setPropertyList(currentPage);
+    };
+
+    useEffect(() => {
+        let ignore = false;
+
+        if (!ignore) {
+            fetch("http://localhost:3000/api/properties").then((response) => {
+                return response.json();
+            }).then((data) => {
+                paginateResults(data, 1);
+            });
+        }        
+
+        return () => {
+            ignore = true;
+        }
+    }, []);
+
     return (
         <ul className="PropertyListing">
-            {Array(5)
-                .fill(DUMMY_PROPERTY)
-                .map((property, index) => (
-                    <li key={index}>
-                        <PropertyCard {...property} />
-                    </li>
-                ))}
+            {propertyList.map((property, index) => (
+                <li key={index}>
+                    <PropertyCard {...property} />
+                </li>
+            ))}
         </ul>
     );
 };
